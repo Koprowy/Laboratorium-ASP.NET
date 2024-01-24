f465a2caef550b2f20fbc153a320321e823e66ba
Krystian Skrzyński <krystian.skrzynski@microsoft.wsei.edu.pl>
2024-01-24 22:02:00 +0100

    Upgrade

---
+++ Laboratorium 3 - App-ns/Controllers/PhotoController.cs
@@ -0,0 +1,60 @@
+﻿using Laboratorium_3___App_ns.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Laboratorium_3___App_ns.Controllers
+{
+    public class PhotoController : Controller
+    {
+        private static List<Photo> photos = new List<Photo>();
+        private static int nextId = 1;
+
+        [HttpPost]
+        public IActionResult Create(Photo photo)
+        {
+            if (ModelState.IsValid)
+            {
+                // Tutaj logika dodawania zdjęcia do bazy danych lub kolekcji
+                return RedirectToAction("Index"); // Przekierowanie do widoku listy zdjęć
+            }
+
+            return View(photo); // Ponowne wyświetlenie formularza z błędami walidacji
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            var photo = photos.FirstOrDefault(p => p.Id == id);
+            if (photo == null)
+            {
+                return NotFound();
+            }
+
+            return View(photo);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            var photo = photos.FirstOrDefault(p => p.Id == id);
+            if (photo == null)
+            {
+                return NotFound();
+            }
+
+            photos.Remove(photo);
+
+            return RedirectToAction("Index");
+        }
+        public IActionResult Details(int id)
+        {
+            var photo = photos.FirstOrDefault(p => p.Id == id);
+            if (photo == null)
+            {
+                return NotFound();
+            }
+
+            return View(photo);
+        }
+
+    }
+}
---
+++ Laboratorium 3 - App-ns/Models/Photo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Laboratorium_3___App_ns.Models
+{
+    public class Photo
+    {
+        [HiddenInput]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Proszę podać datę i godzinę zdjęcia!")]
+        [DataType(DataType.DateTime)]
+        public DateTime DateTaken { get; set; }
+
+        [StringLength(500, ErrorMessage = "Opis jest za długi.")]
+        public string Description { get; set; }
+
+        [Required(ErrorMessage = "Proszę podać nazwę aparatu!")]
+        public string Camera { get; set; }
+
+        [Required(ErrorMessage = "Proszę podać autora zdjęcia!")]
+        public string Author { get; set; }
+
+        [Required(ErrorMessage = "Proszę podać rozdzielczość zdjęcia!")]
+        public string Resolution { get; set; }
+
+        [Required(ErrorMessage = "Proszę podać format zdjęcia!")]
+        public string Format { get; set; }
+    }
+}
